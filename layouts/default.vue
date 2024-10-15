907b00758efcde6c597071edb51f0cf45c7fe588
Light <chittakorn.dev@gmail.com>
2024-10-15 16:17:25 +0700

    add analytiv

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,5 +10,21 @@
 </template>
 
 <script lang="ts" setup>
-
+// Add Google Analytics
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-RYFT053JRB',
+      async: true,
+    },
+    {
+      children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-RYFT053JRB');
+      `,
+    },
+  ],
+});
 </script>
