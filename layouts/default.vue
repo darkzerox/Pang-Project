d92e87120344f6e10d316efdc74fa3bdcb9c559f
Light <chittaks@scg.com>
2024-10-26 20:07:09 +0700

    add ads tag

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,33 +10,46 @@
 </template>
 
 <script lang="ts" setup>
-useHead({
-  script: [
-    {
-      src: 'https://www.googletagmanager.com/gtag/js?id=G-RYFT053JRB',
-      async: true,
-    },
-    {
-      children: `
+useHead(
+  {
+    script: [
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=G-RYFT053JRB',
+        async: true,
+      },
+      {
+        children: `
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
         gtag('config', 'G-RYFT053JRB');
       `,
-    },
-  ],
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: '/favicon/favicon.ico'
-    },
-    {
-      rel: 'icon',
-      type: 'image/png',
-      href: '/favicon/favicon-32x32.png',
-      sizes: '32x32'
-    }
-  ]
-});
+      },
+      {
+        src: 'https://www.googletagmanager.com/gtag/js?id=AW-16706452945',
+        async: true,
+      },
+      {
+        children: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'AW-16706452945');
+      `,
+      },
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon/favicon.ico'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon/favicon-32x32.png',
+        sizes: '32x32'
+      }
+    ]
+  });
 </script>
