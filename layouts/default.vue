4c7a0e606c32abd07b5fbae5d1c419b20836a7d1
Light <chittaks@scg.com>
2024-11-01 13:38:29 +0700

    add tagmanager

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
+
+
+    <!-- Google Tag Manager (noscript) -->
+    <noscript>
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDC8K265" height="0" width="0"
+        style="display:none;visibility:hidden">
+      </iframe>
+    </noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <NuxtSnackbar />
+
+
     <Header />
     <slot />
     <Footer />
@@ -36,6 +48,13 @@ useHead(
         gtag('js', new Date());
         gtag('config', 'AW-16706452945');
       `,
+      },
+      {
+        children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PDC8K265');`,
       },
     ],
     link: [
